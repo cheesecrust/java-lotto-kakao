31927c0f801a868a5888e8da00d7bc992c25b895
cheesecrust <jack0810@naver.com>
2026-02-11 13:37:21 +0900

    refactor: 최대 재시도 횟수와 탈출조건을 추가했습니다.

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -12,26 +12,35 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class LottoController {
+    private static final Integer RETRY_ATTEMPT = 10;
+
     public static void run() {
-        Money userMoney = retry(InputView::inputMoney);
-        LottoTickets lottoTickets = new LottoTickets(userMoney);
+        try {
+            Money userMoney = retry(InputView::inputMoney);
+            LottoTickets lottoTickets = new LottoTickets(userMoney);
 
-        OutputView.printLottos(lottoTickets.getLottos());
-        Lotto winningLotto = retry(InputView::inputWinningNumbers);
-        LottoNumber bonusNumber = retry(() -> InputView.inputBonusNumber(winningLotto));
+            OutputView.printLottos(lottoTickets.getLottos());
+            Lotto winningLotto = retry(InputView::inputWinningNumbers);
+            LottoNumber bonusNumber = retry(() -> InputView.inputBonusNumber(winningLotto));
 
-        List<Rank> ranks = lottoTickets.match(winningLotto, bonusNumber);
-        OutputView.printWinning(ranks);
-        OutputView.printRate(userMoney.calculateRate(ranks));
+            List<Rank> ranks = lottoTickets.match(winningLotto, bonusNumber);
+            OutputView.printWinning(ranks);
+            OutputView.printRate(userMoney.calculateRate(ranks));
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+            System.out.println("로또가 종료되었습니다.");
+        }
     }
 
     private static <T> T retry(Supplier<T> supplier) {
-        while (true) {
+        int attempt = 0;
+        while (attempt++ < RETRY_ATTEMPT) {
             try {
                 return supplier.get();
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] " + e.getMessage());
             }
         }
+        throw new IllegalStateException("최대 재시도 횟수를 초과했습니다.");
     }
 }
